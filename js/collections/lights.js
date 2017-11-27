356484e5f4e9d6532ad75ecd69f9dfcf47a99c32
Hristo Yankov <hristo.yankov@bitcurb.com>
2017-11-27 11:58:43 +0000

    state machine

--- js/collections/lights.js
+++ js/collections/lights.js
@@ -21,11 +21,76 @@ app.Collections = app.Collections || {};
                 // Blinking Yellow
                 new app.Models.LightStateMachine({
                     light: new app.Models.Light({ color: "yellow" }),
-                    interval: 0.8
+                    states: [
+                        { isOn: true, lengthInSeconds: 1 },
+                        { isOn: false, lengthInSeconds: 1 }
+                    ]
                 }),
 
                 // Off
                 new app.Models.LightStateMachine()
+            ]);
+        },
+
+        setInStandardOperational: function () {
+            this.reset([
+                // Red
+                new app.Models.LightStateMachine({
+                    light: new app.Models.Light({ color: "red" }),
+                    states: [
+                        // Red for 8s
+                        { isOn: true, lengthInSeconds: 8 },
+
+                        // Red+Yellow for 2s
+                        { isOn: true, lengthInSeconds: 2 },
+
+                        // Green for 10s
+                        { isOn: false, lengthInSeconds: 10 },
+
+                        // Yellow for 2s
+                        { isOn: false, lengthInSeconds: 2 }
+                    ]
+                }),
+
+                // Yellow
+                new app.Models.LightStateMachine({
+                    light: new app.Models.Light({ color: "yellow" }),
+                    states: [
+                        // Red for 8s
+                        { isOn: false, lengthInSeconds: 8 },
+
+                        // Red+Yellow for 2s
+                        { isOn: true, lengthInSeconds: 2 },
+
+                        // Green for 10s
+                        { isOn: false, lengthInSeconds: 10 },
+
+                        // Yellow for 2s
+                        { isOn: true, lengthInSeconds: 2 }
+                    ]
+                }),
+
+                // Green
+                new app.Models.LightStateMachine({
+                    light: new app.Models.Light({ color: "green" }),
+                    states: [
+                        // Red for 8s
+                        { isOn: false, lengthInSeconds: 8 },
+
+                        // Red+Yellow for 2s
+                        { isOn: false, lengthInSeconds: 2 },
+
+                        // Green for 6s + 4s blink
+                        { isOn: true, lengthInSeconds: 6 },
+                        { isOn: false, lengthInSeconds: 1 },
+                        { isOn: true, lengthInSeconds: 1 },
+                        { isOn: false, lengthInSeconds: 1 },
+                        { isOn: true, lengthInSeconds: 1 },
+
+                        // Yellow for 2s
+                        { isOn: false, lengthInSeconds: 2 }
+                    ]
+                })
             ]);
         }
     });
